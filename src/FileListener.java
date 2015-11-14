92cce7c19635b3e747ce77231034245984f235d1
Bradley Hodgkins <bzh21@bath.ac.uk>
2015-11-14 17:34:50 +0000

    Now while(true) for file reading

--- src/FileListener.java
+++ src/FileListener.java
@@ -5,28 +5,29 @@ import java.io.IOException;
 
 class FileListener extends Thread{
 	SudokuJavaFx app;
-	
+
 	public void setApp(SudokuJavaFx newApp){
 		app = newApp;
 	}
-	
+
 	public void run() {
-
-
 		//File reading and game loop
 		try {
 			File file = new File("./command_list.txt");
 			FileReader fr = new FileReader(file);
 			BufferedReader br = new BufferedReader(fr);
 			String line;
-			while((line = br.readLine()) != null) {
-				System.out.println("HI MATT YOU TWAT ps i'm reading");
-				app.handleCommand(line);
+			while(true) {
+				line = br.readLine();
+				System.out.println("Reading Line");
+
+				if(line != null)
+					app.handleCommand(line);
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
+					break;
 				}
 			}
 			br.close();
